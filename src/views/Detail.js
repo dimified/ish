92883b042ccdcd1883005478c64ee6773d87dedd
Dimitri Reifschneider <dimitri.reifschneider@gmail.com>
2017-05-13 18:59:42 +0200

    Adding image and headline

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default class Home extends Component {
     static navigationOptions = {
         title: 'Detail'
     };
+
     render() {
         return (
-            <View>
-                <Text>Detail view</Text>
+            <View style={ styles.container }>
+                <Image
+                    source={{ uri: 'http://placehold.it/500x300' }}
+                    style={ styles.image }
+                />
+                <Text style={ styles.headline }>Headline</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "stretch"
+    },
+    image: {
+        flex: 1,
+        width: null,
+        height: null
+    },
+    headline: {
+        flex: 1,
+        fontSize: 30
+    }
+});
